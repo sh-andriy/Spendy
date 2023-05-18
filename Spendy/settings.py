24067e1351cd7160bf9c63728759ea622e6959d1
sh-andriy <sheredko.andriy@gmail.com>
2023-05-18 21:39:44 +0300

    v0.7.5
    - testing tweaked app for heroku

--- Spendy/settings.py
+++ Spendy/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import django_on_heroku
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -50,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Spendy.urls'
@@ -127,6 +131,8 @@ STATICFILES_DIRS = [
     BASE_DIR / 'essentials/static'
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
@@ -137,3 +143,5 @@ AUTH_USER_MODEL = 'users.User'
 LOGIN_URL = 'users:login'
 
 LOGIN_REDIRECT_URL = "/"
+
+django_on_heroku.settings(locals())
