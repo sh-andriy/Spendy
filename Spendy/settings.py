2ab9f1d0595e83addc751db219c0b7e1de5cb4fa
Oleh Rylskyj <olehrylskyj@gmail.com>
2023-05-18 19:57:52 +0300

    some extra settings

--- Spendy/settings.py
+++ Spendy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(%d%-$mzz4_t@+e_7(w85@rrpe+5u7fj+*7-*(oem%m!xo^+&d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
